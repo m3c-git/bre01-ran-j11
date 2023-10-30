d9b7c24067920bf078a2ae8ead514b6e0af2ee82
m3c-git <ayyoubentreprise@gmail.com>
2023-10-30 15:48:08 +0000

    update ex 5

--- ex-shell/ex5.sh
+++ ex-shell/ex5.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 mkdir exercice5
-mkdir exercice5/dos1 && mkdir exercice5/dos2
-touch exercice5/dos1/fic11.txt && touch exercice5/dos1/fic12.txt
-touch exercice5/dos2/fic21.txt && touch exercice5/dos2/fic22.txt
+mkdir exercice5/dc && mkdir exercice5/marvel
+touch exercice5/dc/batman.txt && touch exercice5/dc/superman.txt
+touch exercice5/marvel/captain_america.txt && touch exercice5/marvel/iron_man.txt
+echo "Bruce Wayne" >> exercice5/dc/batman.txt && echo "Clark Kent" >> exercice5/dc/superman.txt
+echo "Steve Rogers" >> exercice5/marvel/captain_america.txt && echo "Tony Stark" >> exercice5/marvel/iron_man.txt
