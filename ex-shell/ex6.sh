36367eed1d920e05293b68c4befa7f5f77e94a3a
m3c-git <ayyoubentreprise@gmail.com>
2023-10-30 15:28:51 +0000

    add ex 6

--- ex-shell/ex6.sh
+++ ex-shell/ex6.sh
@@ -8,4 +8,38 @@ echo "Quelle est le nom du projet ?"
 read projectName
 
 
-mkdir site/
+mkdir /home/eddyfrair/sites/$projectType && mkdir /home/eddyfrair/site/$projectType/$projectName
+
+if ["$projectType" == "inte"] 
+then
+    mkdir /home/eddyfrair/sites/$projectType/assets
+    mkdir /home/eddyfrair/sites/$projectType/assets/css
+    mkdir /home/eddyfrair/sites/$projectType/assets/js
+    mkdir /home/eddyfrair/sites/$projectType/assets/img
+    cp /home/eddyfrair/index.html /home/eddyfrair/sites/$projectType/index.html
+    cp /home/eddyfrair/style.css /home/eddyfrair/sites/$projectType/assets/css/style.css
+    
+fi
+
+if ["$projectType" == "php"] 
+then
+    mkdir /home/eddyfrair/sites/$projectType/assets
+    mkdir /home/eddyfrair/sites/$projectType/assets/css
+    mkdir /home/eddyfrair/sites/$projectType/assets/js
+    mkdir /home/eddyfrair/sites/$projectType/assets/img
+    cp /home/eddyfrair/index.php /home/eddyfrair/sites/$projectType/index.php
+fi
+
+if ["$projectType" == "js"] 
+then
+    mkdir /home/eddyfrair/sites/$projectType/assets
+    mkdir /home/eddyfrair/sites/$projectType/assets/css
+    mkdir /home/eddyfrair/sites/$projectType/assets/js
+    mkdir /home/eddyfrair/sites/$projectType/assets/js/modules
+    mkdir /home/eddyfrair/sites/$projectType/assets/img
+    cp /home/eddyfrair/index.html /home/eddyfrair/sites/$projectType/index.html
+    cp /home/eddyfrair/style.css /home/eddyfrair/sites/$projectType/assets/css/style.css
+    cp /home/eddyfrair/main.js /home/eddyfrair/sites/$projectType/assets/js/main.js
+
+fi
+    
